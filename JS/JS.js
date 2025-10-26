839aaba0c82413466cfa7719048017e2cb519406
SY712 <c3p31048@bunkyo.ac.jp>
2025-10-26 16:24:30 +0900

    点検チェックシート画面CSS作成、HTML・JS編集

--- JS/JS.js
+++ JS/JS.js
@@ -1,3 +1,4 @@
+// ログインボタン
 document.addEventListener("DOMContentLoaded", function () {
     const photoButton = document.getElementById("photoButton");
     const photoButtonContainer = document.getElementById("photoButtonContainer");
@@ -8,3 +9,14 @@ document.addEventListener("DOMContentLoaded", function () {
         newButtonsContainer.style.display = "block";
     });
 });
+
+// 点検チェックシート画面　日付入力欄
+window.onload = function(){
+    var getToday = new Date();
+    var y = getToday.getFullYear();
+    var m = getToday.getMonth() + 1;
+    var d = getToday.getDate();
+    var today = y + "-" + m.toString().padStart(2,'0') + "-" + d.toString().padStart(2,'0');
+    document.getElementById("datepicker2").setAttribute("value",today);
+    document.getElementById("datepicker2").setAttribute("min",today);
+}
